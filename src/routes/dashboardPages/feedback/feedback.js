662c1ca939c570077576157f20d0a9c6ffa90f68
BRX0102 <brono@csumb.edu>
2017-03-29 12:42:18 -0700

    added feedback page

---
+++ src/routes/dashboardPages/feedback/feedback.js
@@ -0,0 +1,23 @@
+/**
+ * Created by BRX01 on 3/29/2017.
+ */
+/**
+ * Created by BRX01 on 3/29/2017.
+ */
+
+import React, { Component } from 'react';
+import { PageHeader, Table } from 'react-bootstrap';
+
+const title = 'Feedback';
+
+class Feedback extends Component{
+  render(){
+    return(
+      <div>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfOfULNtRb63ZhO8Rw1ITRUaZpYaE8V4xZGAcHr6jHAeBLkqw/viewform?embedded=true" width="900" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
