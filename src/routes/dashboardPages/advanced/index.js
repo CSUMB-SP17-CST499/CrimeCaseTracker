dbbe856ddf0d2795723a3e6c4b38ff22f0d133f7
BRX0102 <brono@csumb.edu>
2017-04-03 17:25:36 -0700

    base for advanced

---
+++ src/routes/dashboardPages/advanced/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Brian on 4/3/2017.
+ */
+import React from 'react';
+import Advanced from './advanced';
+
+export default {
+  path: '/advanced',
+
+  action() {
+    return (
+      <div>
+        <Advanced />
+      </div>
+    );
+  },
+
+};
