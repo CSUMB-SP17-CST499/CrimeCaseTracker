71488785d0986aaf5559845a9d6158c9e3de6eb4
Ernesto Cortez <ernestoluiscortez@gmail.com>
2017-04-07 16:55:32 -0700

    Add cosnole log to demo how to use fetch function

--- src/routes/home/Dashboard.js
+++ src/routes/home/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, ButtonToolbar, Table, PageHeader } from 'react-bootstrap';
-import { AddNewCaseButton, CaseRow } from './EditCases.js'
+import { AddNewCaseButton, CaseRow } from './EditCases.js';
+import getUserCases from '../../../src/public/fetchDB';
 
 export default class Dashboard extends Component{
   constructor() {
@@ -33,7 +34,9 @@ export default class Dashboard extends Component{
         }
       );
     }
-
+    getUserCases('dude').then((cases) => {
+      console.log(cases);
+    });
     this.setState({cases: tempCases});
   }
   addCase(newCase){
@@ -132,7 +135,7 @@ class CaseSearchBox extends Component{
 
   render(){
     return (
-      <input type="text" id="myInput" onKeyUp={()=>this.autoSearch()} placeholder="Search..." />
+      <input type="text" id="myInput" onKeyUp={() => this.autoSearch()} placeholder="Search..." />
     );
   }
 }
