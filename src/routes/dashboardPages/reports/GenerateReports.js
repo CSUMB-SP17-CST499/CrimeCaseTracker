98f2acebc4f36a2b09c762dbf07b2c88231ef4ba
alexanderDoria <adoria@csumb.edu>
2017-05-07 00:46:24 -0700

    recharts to generate sample report

--- src/routes/dashboardPages/reports/GenerateReports.js
+++ src/routes/dashboardPages/reports/GenerateReports.js
@@ -1,62 +1,72 @@
 /**
  * Created by Red-8 on 4/7/2017.
  */
+/* global Plotly */
+// Plot.js
 import React, { Component } from 'react';
+import { getAllFromTable } from '../../../public/fetchDB';
+import {LineChart, Bar, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart} from 'Recharts';
+
+const data2 = [
+  {name: 'stabbing', 2015: 12, 2016: 14, amt: 2400},
+  {name: 'shooting', 2015: 6, 2016: 4, amt: 2210},
+  {name: 'suicide', 2015: 3, 2016: 4, amt: 2290}
+];
+const SimpleBarChart = React.createClass({
+  render () {
+    return (
+      <BarChart width={600} height={300} data={data2}
+                margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+        <XAxis dataKey="name"/>
+        <YAxis/>
+        <CartesianGrid strokeDasharray="3 3"/>
+        <Tooltip/>
+        <Legend />
+        <Bar dataKey="2015" fill="#8884d8" />
+        <Bar dataKey="2016" fill="#82ca9d" />
+      </BarChart>
+    );
+  }
+})
 
 export default class GenerateReports extends Component{
   constructor() {
     super()
     this.state = {
-      cases: [],
-      suspects: [],
-      locations: [],
-      caseNums: [],
-      statuses: []
+      cases:[]
     }
+  }
+  componentWillMount(){
+    var plotScript = document.createElement('script');
+    plotScript.setAttribute('src','https://cdn.plot.ly/plotly-latest.min.js');
+    document.head.appendChild(plotScript);
+
   }
   componentDidMount(){
-    this.loadFromServer();
-  }
-  loadFromServer(){
-    var N = 8;
-    var tempCases = [];
-    this.state.caseNums = Array.apply(null, {length: N}).map(Number.call, Number);
-    this.state.suspects = ["Alfreds Futterkiste", "Berglunds snabbkop", "Island Trading", "Koniglich Essen", "Laughing Bacchus Winecellars", "Magazzini Alimentari Riuniti", "North/South", "Paris specialites"];
-    this.state.locations = ["Germany", "Sweden", "UK", "Germany", "Canada", "Italy", "UK", "France"];
-    this.state.statuses = ["exclamation-sign", "exclamation-sign", "exclamation-sign", "envelope", "envelope", "check", "check", "check"];
+    getAllFromTable('Casetrack').then((cases) => {
+      console.log(cases);
+      this.setState({cases: cases});
+    });
 
-    for (var i = 0; i < 8; i++) {
-      tempCases.push({
-          id: this.state.caseNums[i],
-          suspect: this.state.suspects[i],
-          location: this.state.locations[i],
-          status: this.state.statuses[i]
-        }
-      );
+    console.log("hello");
+    var obj = { };
+    for (var i = 0; i < this.state.cases.length; i++) {
+      obj[arr[i]] = (obj[arr[i]] || 0) + 1;
+      console.log("i:" + i);
     }
 
-    this.setState({cases: tempCases});
-  }
-  addCase(newCase){
-    //new case will have last used id + 1, will be changed when "actual" IDs are added
-    newCase.id = this.state.caseNums.length;
+    console.log("obj");
+    console.log(obj);
 
-    this.setState({cases: this.state.cases.concat(newCase)});
-  }
-  updateCase(newCase){
-    //update case at index - directly to AWS server
-    console.log("updating case... ");
-
-    //update case on dashboard
-    this.state.cases[newCase.id] = newCase;
-    this.setState({cases: this.state.cases});
   }
   render(){
-    let add = (c) => this.addCase(c);
     return(
-      <div>
-        render stats methods
+      <div id="myDiv">
+        <div style={{"margin": "40px"}}>
+          <SimpleBarChart/>
+        </div>
       </div>
     );
   }
 }
+
